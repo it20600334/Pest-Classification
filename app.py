fe089f443902fd9bb245f3af731c59b8fbbe73b4
IT20600334 - Eranda Withanage <88669615+it20600334@users.noreply.github.com>
2024-03-11 22:53:09 +0530

    commit 2

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 # import flast module
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image as keras_image
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import cv2
 
 # instance of flask application
 app = Flask(__name__)
 
+# Load the model
+# model_path = '/path/to/your/model.h5'  # Replace with your model path
+# model = load_model(model_path)
+# class_labels = ['CCI', 'NI', 'WF']  # Replace with your class labels
+
+def preprocess_image(img):
+    # Preprocess the image (resize, preprocess_input)
+    img = cv2.resize(img, (224, 224))  # Adjust target_size if needed
+    img_array = keras_image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+    return img_array
+
+
+
 # home route that returns below text when root url is accessed
 @app.route("/")
-def hello_world():
-	return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__': 
     app.run()
